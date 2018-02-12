002a8a2b23256d57f4c53789079c1d8d5a7ee7a9
Haowei Qu <33754540+CorbieInGZF@users.noreply.github.com>
2018-02-12 20:08:10 +0800

    18.2.12

---
+++ f.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-02-10 08:55:04
+ * @version $Id$
+ */
+function firstTest(){
+	document.getElementById("firsttest").innerHTML="第一个外部javascript";
+}
